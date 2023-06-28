51b85e788c98169f74209cdf981951ebd2cda3d4
Ricardo-Lucena-Clear <ricardo.lucena.clear@gmail.com>
2023-06-28 14:20:53 -0300

    Component with Properties

--- App.js
+++ App.js
@@ -2,13 +2,17 @@ import React from 'react';
 import { Text,  View, StyleSheet } from 'react-native';
 import Primeiro from "./src/components/Primeiro"
 import  CompPadrao , {Comp1, Comp2} from "./src/components/Multi"
+import MinMax from './src/components/MinMax';
 
 export default () => (
   <View style={style.App}>
+    <MinMax min= "3" max= "20"/>
+    <MinMax min= "1" max= "94"/>
+  { /*
     <CompPadrao/>
     <Comp1/>
     <Comp2/>
-    <Primeiro/>
+    <Primeiro/>*/}
   </View>
 )
 
@@ -18,6 +22,7 @@ const style = StyleSheet.create ({
         flexGrow: 1, /* usado para preencher a tela toda(cor no caso)*/
         justifyContent: 'center', /* usado para alterar posição na vertical*/
         alignItems: 'center', /* usado para alterar posição na horizontal*/
+        padding: 20
     }
 })
 
--- src/components/estilo.js
+++ src/components/estilo.js
@@ -2,6 +2,7 @@ import { StyleSheet } from "react-native";
 
 export default StyleSheet.create({
     TxtG: {
-        fontSize: 25
+        fontSize: 25,
+        textAlign: 'center'
     }
 })
