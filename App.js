d8a484f5d9c3cf1079f333efd95e84ced7b2d74c
Ricardo-Lucena-Clear <ricardo.lucena.clear@gmail.com>
2023-06-28 13:17:24 -0300

    ECMAScript Module System

--- App.js
+++ App.js
@@ -1,9 +1,13 @@
 import React from 'react';
 import { Text,  View } from 'react-native';
 import Primeiro from "./src/components/Primeiro"
+import  CompPadrao , {Comp1, Comp2} from "./src/components/Multi"
 
 export default () => (
   <View>
+    <CompPadrao/>
+    <Comp1/>
+    <Comp2/>
     <Primeiro/>
   </View>
 )
